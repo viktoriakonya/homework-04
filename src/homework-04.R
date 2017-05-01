a6e371186f3244aa79b617802b0f56fa62484af3
viktoriakonya <viktoriakonya@gmial.com>
2017-05-01 18:17:43 +0200

    3. feladat is többé-kevésbé kész.

--- src/homework-04.R
+++ src/homework-04.R
@@ -161,3 +161,290 @@ num_tweets("Donald Trump", 15)
 
 # Ez egy helytelen név lesz
 num_tweets("Helytelen Név", 10)
+
+
+
+
+################################################################################
+--------------------------------------------------------------------------------
+################################################################################
+
+# 3. Feladat
+
+
+# 3. Feladat -------------------------------------------------------------------
+
+rm(list=ls(all=TRUE))
+
+### dataset behívása
+require(fivethirtyeight)
+data("hiphop_cand_lyrics")
+View(hiphop_cand_lyrics)
+as.data.frame(hiphop_cand_lyrics)
+
+# Package-ek behívása
+library(ggplot2)
+library(gcookbook)
+install.packages("cowplot")
+devtools::install_github("wilkelab/cowplot")
+library("cowplot")
+library(gridExtra)
+
+
+# Név szétbontása
+hiphop_cand_lyrics$name2 <- strsplit(as.character(hiphop_cand_lyrics$candidate), split = " ") 
+View(hiphop_cand_lyrics)
+hiphop_cand_lyrics$name2[[1]][[1]]
+
+for (i in 1:length(hiphop_cand_lyrics$name2)){
+  hiphop_cand_lyrics$name2_pri[i] <- hiphop_cand_lyrics$name2[[i]][[2]]
+}
+View(hiphop_cand_lyrics)
+
+# Fig könyvtár létrehozása
+#dir.create("fig", showWarnings = FALSE)
+
+### Külön subseteket készítettem a feladatrészekhez
+hiphop_cand_lyrics$num <- c(1)
+# 3.1. ábrához
+subset1 <- aggregate(hiphop_cand_lyrics$num, by = list(date = hiphop_cand_lyrics$album_release_date, name=hiphop_cand_lyrics$name2_pri), sum)
+as.data.frame(subset1)
+View(subset1)
+# 3.2. ábrához
+subset2<-aggregate(hiphop_cand_lyrics$num, by = list(date = hiphop_cand_lyrics$album_release_date, name=hiphop_cand_lyrics$name2_pri, sentiment=hiphop_cand_lyrics$sentiment), sum)
+as.data.frame(subset2)
+View(subset2)
+# 3.3. ábrához
+subset3<-aggregate(hiphop_cand_lyrics$num, by = list(date = hiphop_cand_lyrics$album_release_date, name=hiphop_cand_lyrics$name2_pri, theme=hiphop_cand_lyrics$theme), sum)
+as.data.frame(subset3)
+View(subset3)
+
+### 3.1. ábra
+# nem jó a label
+subset1$name <- as.factor(subset1$name)
+levels(subset1$name)
+subset1$name = factor(subset1$name,levels(subset1$name)[c(8, 4, 1,3,6,7,2,5)])
+subset1$name <- factor(subset1$name, levels = rev(levels(subset1$name)))
+levels(subset1$name)
+View(subset1)
+
+
+plot_1 <- ggplot(subset1[order(subset1$name),], aes(x = subset1$date, y = subset1$x, fill = subset1$name)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  scale_fill_manual("", values = c("Trump" = "burlywood1", "Clinton" = "cadetblue2", "Bush" = "coral2", "Huckabee"="antiquewhite1", "Cruz"="deeppink3", "Sanders"="darkseagreen4", "Christie"="darkgoldenrod1", "Carson"="darkolivegreen3" )) +
+  theme(
+    axis.title.x=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.title.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    panel.grid.major = element_line( size=.1, color="grey"),
+    panel.grid.minor = element_line( size=.1, color="grey") 
+  )+
+  scale_x_continuous( breaks=c(seq(1990,2016,by=5)),
+                      minor_breaks=seq(1990,2016,by=5) 
+  )+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+plot_1 + theme(legend.position="top") +
+  ggtitle("Every mention of 2016 primary candidates in hip-hop songs")
+
+ggsave("fig/hiphop1.png", width = 8, height = 5, dpi = 100)
+
+### 3.2. ábra
+
+# Szétbontás érzelmek szerint subsetekre
+subset21 <-subset(subset2, subset2$sentiment=="positive")
+subset22 <-subset(subset2, subset2$sentiment=="negative")
+subset23 <-subset(subset2, subset2$sentiment=="neutral")
+
+View(subset21)
+View(subset22)
+View(subset23)
+
+### 3.2.1. ábra
+subset21$name <- as.factor(subset21$name)
+levels(subset21$name)
+subset21$name = factor(subset21$name,levels(subset21$name)[c(4, 2, 3,1)])
+subset21$name <- factor(subset21$name, levels = rev(levels(subset21$name)))
+levels(subset21$name)
+View(subset21)
+
+plot_21 <- ggplot(subset21[order(subset21$name),], aes(x = subset21$date, y = subset21$x, fill = subset21$name)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  scale_fill_manual("", values = c("Trump" = "burlywood1", "Clinton" = "cadetblue2", "Bush" = "coral2", "Huckabee"="antiquewhite1", "Cruz"="deeppink3", "Sanders"="darkseagreen4", "Christie"="darkgoldenrod1", "Carson"="darkolivegreen3" )) +
+  theme(
+    axis.title.x=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.title.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    panel.grid.major = element_line( size=.1, color="grey"),
+    panel.grid.minor = element_line( size=.1, color="grey") 
+  )+
+  theme(plot.title = element_text(hjust = 0.5))
+
+plot_21
+plot_21 + theme(legend.position="top") 
+
+### 3.2.2. ábra
+subset22$name <- as.factor(subset22$name)
+levels(subset22$name)
+subset22$name = factor(subset22$name,levels(subset22$name)[c(6, 2, 1,4,5,3)])
+subset22$name <- factor(subset22$name, levels = rev(levels(subset22$name)))
+levels(subset22$name)
+View(subset22)
+
+plot_22 <- ggplot(subset22[order(subset22$name),], aes(x = subset22$date, y = subset22$x, fill = subset22$name)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  scale_fill_manual("", values = c("Trump" = "burlywood1", "Clinton" = "cadetblue2", "Bush" = "coral2", "Huckabee"="antiquewhite1", "Cruz"="deeppink3", "Sanders"="darkseagreen4", "Christie"="darkgoldenrod1", "Carson"="darkolivegreen3" )) +
+  theme(
+    axis.title.x=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.title.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    panel.grid.major = element_line( size=.1, color="grey"),
+    panel.grid.minor = element_line( size=.1, color="grey") 
+  )+
+  theme(plot.title = element_text(hjust = 0.5))
+
+plot_22
+plot_22 + theme(legend.position="top")
+
+
+### 3.2.3. ábra
+subset23$name <- as.factor(subset23$name)
+levels(subset23$name)
+subset23$name = factor(subset23$name,levels(subset23$name)[c(5,3,1,2,4)])
+subset23$name <- factor(subset23$name, levels = rev(levels(subset23$name)))
+levels(subset23$name)
+View(subset23)
+
+plot_23 <- ggplot(subset23[order(subset23$name),], aes(x = subset23$date, y = subset23$x, fill = subset23$name)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  scale_fill_manual("", values = c("Trump" = "burlywood1", "Clinton" = "cadetblue2", "Bush" = "coral2", "Huckabee"="antiquewhite1", "Cruz"="deeppink3", "Sanders"="darkseagreen4", "Christie"="darkgoldenrod1", "Carson"="darkolivegreen3" ))+
+  theme(
+    axis.title.x=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.title.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    panel.grid.major = element_line( size=.1, color="grey"),
+    panel.grid.minor = element_line( size=.1, color="grey") 
+  )+
+  theme(plot.title = element_text(hjust = 0.5))
+
+plot_23
+plot_23 + theme(legend.position="top") 
+
+# 3.2.1.-3.2.3. egyben
+#install.packages("cowplot")
+#devtools::install_github("wilkelab/cowplot")
+#library("cowplot")
+#library(gridExtra)
+
+#list=c("Positive","Negative", "Neutral")
+
+plot_egyben<-plot_grid(plot_21, plot_22, plot_23, ncol=3,
+                       labels=c("Positive","Negative", "Neutral"), label_size=12, align="hv")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+plot_egyben + theme(legend.position="top") +
+  ggtitle("Candidate mentions, by sentiment")
+
+ggsave("fig/hiphop2.png", width = 20, height = 5, dpi = 100)
+
+
+### 3.3. ábra
+# Szétbontás téma szerint subsetekre
+subset31 <-subset(subset3, subset3$theme=="money")
+subset32 <-subset(subset3, subset3$theme=="hotel")
+subset33 <-subset(subset3, subset3$theme=="political")
+
+View(subset31)
+View(subset32)
+View(subset33)
+
+### 3.3.1 ábra
+subset31$name <- as.factor(subset31$name)
+levels(subset31$name)
+subset31$name = factor(subset21$name,levels(subset21$name)[c(2, 1)])
+subset31$name <- factor(subset21$name, levels = rev(levels(subset21$name)))
+levels(subset31$name)
+View(subset31)
+
+plot_31 <- ggplot(subset31[order(subset31$name),], aes(x = subset31$date, y = subset31$x, fill = subset31$name)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  scale_fill_manual("", values = c("Trump" = "burlywood1", "Clinton" = "cadetblue2", "Bush" = "coral2", "Huckabee"="antiquewhite1", "Cruz"="deeppink3", "Sanders"="darkseagreen4", "Christie"="darkgoldenrod1", "Carson"="darkolivegreen3" )) +
+  theme(
+    axis.title.x=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.title.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    panel.grid.major = element_line( size=.1, color="grey"),
+    panel.grid.minor = element_line( size=.1, color="grey") 
+  )
+
+plot_31 + theme(legend.position="top") 
+
+
+### 3.2.2. ábra
+subset32$name <- as.factor(subset32$name)
+levels(subset32$name)
+subset32$name = factor(subset32$name,levels(subset32$name)[c(1)])
+subset32$name <- factor(subset32$name, levels = rev(levels(subset32$name)))
+levels(subset32$name)
+View(subset32)
+
+plot_32 <- ggplot(subset22[order(subset32$name),], aes(x = subset32$date, y = subset32$x, fill = subset32$name)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  scale_fill_manual("", values = c("Trump" = "burlywood1", "Clinton" = "cadetblue2", "Bush" = "coral2", "Huckabee"="antiquewhite1", "Cruz"="deeppink3", "Sanders"="darkseagreen4", "Christie"="darkgoldenrod1", "Carson"="darkolivegreen3" )) +
+  theme(
+    axis.title.x=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.title.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    panel.grid.major = element_line( size=.1, color="grey"),
+    panel.grid.minor = element_line( size=.1, color="grey") 
+  )
+
+plot_32
+plot_32 + theme(legend.position="top") 
+
+### 3.2.3. ábra
+subset33$name <- as.factor(subset33$name)
+levels(subset33$name)
+subset33$name = factor(subset33$name,levels(subset33$name)[c(5,2,1,4,3)])
+subset33$name <- factor(subset33$name, levels = rev(levels(subset33$name)))
+levels(subset33$name)
+View(subset33)
+
+plot_33 <- ggplot(subset33[order(subset33$name),], aes(x = subset33$date, y = subset33$x, fill = subset33$name)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  scale_fill_manual("", values = c("Trump" = "burlywood1", "Clinton" = "cadetblue2", "Bush" = "coral2", "Huckabee"="antiquewhite1", "Cruz"="deeppink3", "Sanders"="darkseagreen4", "Christie"="darkgoldenrod1", "Carson"="darkolivegreen3" ))+
+  theme(
+    axis.title.x=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.title.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    panel.grid.major = element_line( size=.1, color="grey"),
+    panel.grid.minor = element_line( size=.1, color="grey") 
+  )
+
+plot_33
+plot_33 + theme(legend.position="top") 
+
+# 3.3.1.-3.3.3. Egyben
+plot_egyben2<-plot_grid(plot_31, plot_32, plot_33, ncol=3,
+                        labels=c("Wealth","Trump Tower", "Politics"), label_size=12, align="hv")
+
+
+plot_egyben2 + theme(legend.position="top") +
+  ggtitle("Candidate mentions, by subject")
+
+ggsave("fig/hiphop3.png", width = 20, height = 5, dpi = 100)
+
+
+
+
+
+
+
